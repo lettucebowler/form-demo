332416097acaa47ca99be74943cbcaf4e4e3e07e
Grant Montgomery <lettucebowler@gmail.com>
2022-11-07 01:13:52 -0600

    bleh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
 	import type { ActionData } from './$types';
 	import BetterForm from '$lib/components/BetterForm.svelte';
 	import { writable, type Writable } from 'svelte/store';
-	import { onMount } from 'svelte';
-	// import SimpleForm from '$lib/components/SimpleForm.svelte';
 
 	export let form: ActionData;
 
@@ -18,4 +16,4 @@
 	$: console.log(form);
 </script>
 
-<BetterForm validationState={validationErrors} message={form?.message} input={form?.input} />
+<BetterForm validationState={validationErrors} message={form?.message} input={form?.input || {}} />
